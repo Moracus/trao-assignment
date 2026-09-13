99398021d218f2dd4cb2e2b2cc4ee17f40decb91
moracus <hsharshsharma370@gmail.com>
2026-09-13 13:22:05 +0530

    added cors in env

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,15 @@
-import express from "express"
-import helmet from "helmet"
+import express from "express";
+import helmet from "helmet";
 import mongoose from "mongoose";
-import * as dotenv from "dotenv"
-import cors from "cors"
+import * as dotenv from "dotenv";
+import cors from "cors";
 
 import cookieParser from "cookie-parser";
-import authRoutes from "./routes/auth.routes.js"
-import kitRoutes from "./routes/kit.routes.js"
+import authRoutes from "./routes/auth.routes.js";
+import kitRoutes from "./routes/kit.routes.js";
 import { connectDB } from "./config/db.js";
 dotenv.config();
-const app = express()
+const app = express();
 app.use(helmet());
 
 app.use(
@@ -18,24 +18,22 @@ app.use(
       req.rawBody = buf.toString(); // store raw body as string
     },
     limit: "50mb",
-  })
+  }),
 );
 
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }),
+);
 
-
-
-
-
-app.use("/api/auth",authRoutes);
-app.use("/api/kits",kitRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/kits", kitRoutes);
 
 // default get
 app.get("/health", async (req, res) => {
@@ -47,9 +45,7 @@ app.get("/health", async (req, res) => {
 const starServer = () => {
   try {
     connectDB();
-    app.listen(process.env.PORT, () =>
-      console.log("server has been started")
-    );
+    app.listen(process.env.PORT, () => console.log("server has been started"));
   } catch (error) {
     console.log(error);
   }
